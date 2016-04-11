11f5946ca46af2871f3710fe5635afae5febb5a2
Frank Chen <cheng.frank.chen@gmail.com>
2016-04-11 16:45:32 -0400

    LiveDataAggregator: Functionally complete

--- liveDataAggregator/aggregator/aggregator.go
+++ liveDataAggregator/aggregator/aggregator.go
@@ -9,20 +9,40 @@ Provides data manipulation service for incoming map[string]string
 package aggregator
 
 type Aggregator struct {
-	sum map[string]int
+	sum  map[string]int
+	sema chan struct{}
 }
 
 func NewAggregator() *Aggregator {
 	a := new(Aggregator)
 	a.sum = make(map[string]int)
+	a.sema = make(chan struct{}, 1)
+	a.sema <- struct{}{}
+	return a
 }
 
 func (a *Aggregator) Add(newAgg map[string]int) {
+	<-a.sema
+	defer func() {
+		a.sema <- struct{}{}
+	}()
 	for key, value := range newAgg {
 		a.sum[key] += value
 	}
 }
 
 func (a *Aggregator) GetSum() map[string]int {
+	<-a.sema
+	defer func() {
+		a.sema <- struct{}{}
+	}()
 	return a.sum
+}
+
+func (a *Aggregator) Zero() {
+	<-a.sema
+	defer func() {
+		a.sema <- struct{}{}
+	}()
+	a.sum = make(map[string]int)
 }
--- liveDataAggregator/connections/connections.go
+++ liveDataAggregator/connections/connections.go
@@ -9,14 +9,36 @@ Provides TCP / websocket management
 package connections
 
 import (
+	"encoding/json"
+	"fmt"
 	"net"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	newConnections      map[*websocket.Conn]struct{}
+	existingConnections map[*websocket.Conn]struct{}
+	upgrader            websocket.Upgrader
 )
 
 const (
-	network = "net"
+	network = "tcp"
 )
 
-func OpenTCP(address string) net.Conn {
+func init() {
+	newConnections = map[*websocket.Conn]struct{}{}
+	existingConnections = map[*websocket.Conn]struct{}{}
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+}
+
+func OpenTcp(address string) chan map[string]int {
 	address_tcp, err := net.ResolveTCPAddr(network, address)
 	if err != nil {
 		panic(fmt.Sprintf("Connections: Unable to resolve TCP address %s\n", err))
@@ -27,5 +49,97 @@ func OpenTCP(address string) net.Conn {
 		panic(fmt.Sprintf("Connections: unable to create TCP listener %s\n", err))
 	}
 
-	return connection
+	connOutput := make(chan map[string]int)
+
+	go func() {
+		defer connection.Close()
+		decoder := json.NewDecoder(connection)
+		for {
+			var message map[string]int
+			err := decoder.Decode(&message)
+			if err != nil {
+				fmt.Printf("Connections: error parsing into json: %s\n", err)
+				return
+			}
+			connOutput <- message
+		}
+	}()
+
+	return connOutput
+}
+
+func HandleNewWsConnection(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Connections: unable to upgrade websocket connection from %s: %s\n", r.Referer(), err)
+	}
+
+	go func() {
+		defer closeConn(conn)
+		for {
+			_, _, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Sprintf("Connections: Error [%s] at %s, closing socket\n", err, conn.RemoteAddr())
+				break
+			}
+		}
+	}()
+
+	newConnections[conn] = struct{}{}
+}
+
+func PromoteConns(message map[string]int) {
+	for conn, _ := range newConnections {
+		promoteConn(conn, message)
+	}
+}
+
+func promoteConn(c *websocket.Conn, message map[string]int) {
+	err := sendMessage(c, message)
+	if err != nil {
+		fmt.Printf("Connections: Failed to promote connection %s: %s", c, err)
+	}
+	delete(newConnections, c)
+	existingConnections[c] = struct{}{}
+}
+
+func sendMessage(c *websocket.Conn, m map[string]int) error {
+	json, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	err = c.WriteMessage(websocket.TextMessage, json)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Close() {
+	closePool(newConnections)
+	closePool(existingConnections)
+}
+
+func closePool(pool map[*websocket.Conn]struct{}) {
+	for conn, _ := range pool {
+		closeConn(conn)
+	}
+}
+
+func closeConn(c *websocket.Conn) {
+	c.Close()
+	delete(newConnections, c)
+	delete(existingConnections, c)
+}
+
+func Broadcast(message map[string]int) error {
+	var err error
+	for conn, _ := range existingConnections {
+		failed := sendMessage(conn, message)
+		if failed != nil {
+			err = failed
+		}
+	}
+	return err
 }
