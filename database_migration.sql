f1520648edbf9b5c85e1a9f5125de6f453dff37a
Frank Chen <cheng.frank.chen@gmail.com>
2016-07-09 17:15:51 -0600

    General root file cleanup

--- database_migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE treebeer;
-
-use treebeer;
-
-CREATE TABLE event (
-  id             bigint PRIMARY KEY AUTO_INCREMENT,
-  created_at     date NOT NULL,
-  raw_message    json NOT NULL
-);
