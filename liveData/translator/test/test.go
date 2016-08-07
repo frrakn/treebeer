dbed1e3957f3518230cf384c8ae0941f125c0266
Frank Chen <cheng.frank.chen@gmail.com>
2016-08-07 13:02:06 -0400

    Translator: Tester logs JSONs to logs.txt

--- liveData/translator/test/test.go
+++ liveData/translator/test/test.go
@@ -1,12 +1,22 @@
 package main
 
-import "net"
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
 import "bufio"
 
 func main() {
 
 	for {
+		f, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
 		// connect to this socket
 		conn, err := net.Dial("tcp", "localhost:8082")
 		if err != nil {
@@ -15,7 +25,11 @@ func main() {
 		}
 		for {
 			message, _ := bufio.NewReader(conn).ReadString('}')
-			fmt.Println("Message from server: " + message)
+			fmt.Println(message)
+
+			if _, err = f.WriteString(message); err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
