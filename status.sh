76cbcc75550d381b1d2a6bca7616277277616acd
marvin737 <marvin@outlook.com>
2016-08-20 15:24:34 +0000

    Add script to check if processes are running

---
+++ status.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo | ps aux | grep manager
+echo | ps aux | grep cache
+echo | ps aux | grep updater
+echo | ps aux | grep server
+echo | ps aux | grep translator
+echo | ps aux | grep aggregator
